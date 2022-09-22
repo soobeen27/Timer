71b085dbf5fb8988f22efdbbae2f097fe3ed7aef
soobeen27 <youdew27@icloud.com>
2022-09-22 16:39:11 +0900

    timer

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var secondSlider: UISlider!
+    weak var timer: Timer?
+    var audio: SystemSoundID = 1016
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setSlider()
+        
     }
 
+    func setSlider() {
+        secondSlider.minimumValue = 0
+        secondSlider.maximumValue = 60
+        secondSlider.value = 30
+    }
+    
+    @IBAction func secondSliderVC(_ sender: UISlider) {
+        secondLabel.text = String(Int(secondSlider.value))
+    }
+    @IBAction func startButton(_ sender: UIButton) {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true){ timer in
+            self.secondSlider.value -= 1
+            self.secondLabel.text = String(Int(self.secondSlider.value))
+            if self.secondSlider.value <= 0 {
+                AudioServicesPlaySystemSound(self.audio)
+                timer.invalidate()
+            }
+        }
+    }
+    
+    @IBAction func resetButton(_ sender: UIButton) {
+        secondSlider.value = 30
+        secondLabel.text = "초를 선택하세요"
+        timer?.invalidate()
+    }
 
 }
 
